3a2b3e83c60795354c5f29203d629772b1f4751c
DVMaja <majadreilinger@gmail.com>
2023-05-31 09:59:40 +0200

    Jatekter.js alapszerkezet kész

--- JS/Classok/Jatekter.js
+++ JS/Classok/Jatekter.js
@@ -1,11 +1,31 @@
 import Lampa from "./Lampa.js";
 
 class Jatekter {
+    #db; // int felkapcsolt lámpák száma
+    #allapotLISTA = []; 
+    #meret; // int játéktér mérete, lámpák száma, meret*meret
+    #lepes; // int
 
     constructor(){
         new Lampa();
         console.log("Jatekter.js elérhető");
 
+    }
+
+    #setAllapotLISTA(){
+
+    }
+
+    #szomszedokKeresese(id){
+
+    }
+
+    #init(){
+        //új játéktér
+    }
+
+    #ellenorzes(){
+        //megnézi hogy hány lámpa ég még
     }
 }
 export default Jatekter;
