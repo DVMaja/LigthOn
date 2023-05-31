ddd1e634ff85a6115f7e9232b12bbe58b85e5df6
DVMaja <majadreilinger@gmail.com>
2023-05-31 15:25:13 +0200

    gomb elem meg nem működik

--- JS/main.js
+++ JS/main.js
@@ -3,4 +3,10 @@ import Jatekter from "./Classok/Jatekter.js";
 $(function () {
     new Jatekter();
     console.log("Main csatolva");
+    const gombELEM = $("article gomb");
+    //console.log(gombELEM);
+    gombELEM.on("click", () =>{
+        console.log("Rá lehet kattintani a gombra");
+        //ezzel folytat
+    })
 })
