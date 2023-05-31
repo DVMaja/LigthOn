1e1d9778eb75c8a49c61461648dd5e749746b84e
DVMaja <majadreilinger@gmail.com>
2023-05-31 09:59:52 +0200

    Lampa.js alapszerkezet kész

--- JS/Classok/Lampa.js
+++ JS/Classok/Lampa.js
@@ -1,8 +1,21 @@
 class Lampa {
+    #allapot;//boolean fel le kapcsolt e
+    #id; // int lámpa sorszáma
+    #divELEM // html elem
 
-    constructor(){
+    constructor(id, allapot, szuloELEM){
         console.log("Lampa.js elérhető");
 
+    }
+    setAllapot(){
+        this.#szinBeallit();
+    }
+    #szinBeallit(){
+
+    }
+    #kattintasTrigger(){
+        const esemeny = new CustomEvent("kapcsolas", { detail: this });
+        window.dispatchEvent(esemeny);
     }
 }
 export default Lampa;
