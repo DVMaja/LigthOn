f0c6888a715a1ff9c2fb37b184cec31ddbd74d4c
DVMaja <majadreilinger@gmail.com>
2023-06-01 10:48:43 +0200

    nem éri el: a szin, hamis ág megoldasra var

--- JS/Classok/Lampa.js
+++ JS/Classok/Lampa.js
@@ -6,7 +6,7 @@ class Lampa {
     constructor(id, allapot, szuloELEM) {
         this.#id = id;
         this.#allapot = allapot;
-        szuloELEM.append(`<div class="korELEM"><span class="dot"></span></div>`);
+        szuloELEM.append(`<div class="korELEM"></span></div>`);
         //console.log("Lampa.js elérhető");
 
         this.korELEM = $("ideJatekter .korELEM:last-child p");
@@ -14,20 +14,26 @@ class Lampa {
         this.korELEM.on("click", () => {
             this.#kattintasTrigger;
         })
+        this.setAllapot();
     }
     setAllapot() {
-        if (zold) {
-            //sárgára állítja át
-            this.#szinBeallit();
-        } else if (sarga) {
-            this.#szinBeallit();
-            //zöldre állítja át
-
+        if (false) {
+            //this.korELEM.css(`background-color`, `orange`);
+            console.log("látszik a hamis álítás!");
+            this.#szinBeallit("orange");
+            console.log(this.#szinBeallit);
+        } else if (true) {
+            //this.korELEM.css(`background-color`, `darkgreen`);  
+            let beallitandoSzin = "darkgreen";
+            this.#szinBeallit(beallitandoSzin);
+            console.log("látszik az igaz álítás!");
+            console.log(this.#szinBeallit);
         }
 
     }
-    #szinBeallit() {
-
+    #szinBeallit(szin) {    
+        //meg nem éri el a megadott szint    
+        return this.korELEM.css(`background-color`, szin);
 
     }
     #kattintasTrigger() {
