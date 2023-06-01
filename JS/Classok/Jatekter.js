c2ff3628ee31e874e2b95e20c2b26d613fc07eb8
DVMaja <majadreilinger@gmail.com>
2023-06-01 16:37:46 +0200

    pontszámláló működik

--- JS/Classok/Jatekter.js
+++ JS/Classok/Jatekter.js
@@ -1,36 +1,36 @@
 import Lampa from "./Lampa.js";
 
 class Jatekter {
-    #db; // int felkapcsolt lámpák száma
+    #db;
     #allapotLISTA = [];
     #meret = 9;
-    #lepes; // int
+    #lepes;
 
     constructor() {
         this.#lepes = 0;
+        this.#db = 0;
         const szuloELEM = $("article .ideJatekter");
-        
 
-
-        //console.log("Jatekter.js elérhető");
         for (let szamlalo = 0; szamlalo < this.#meret; szamlalo++) {
             const lampa = new Lampa(szamlalo, this.#allapotLISTA[szamlalo], szuloELEM);
             this.#allapotLISTA.push(lampa);
-            this.#init(lampa, szamlalo);
+            this.#init(lampa);
         }
+        console.log("1. lista: ");
         console.log(this.#allapotLISTA);
-        this.#db = 4;
+        this.#db;
 
-
+        this.#ellenorzes();
         $(window).on("kapcsolas", (event) => {
             console.log(event.detail);
+            this.#allapotLISTA[event.detail] = true;
+            console.log(this.#allapotLISTA[event.detail]);
             this.#setAllapotLISTA(event.detail);
-
-            //console.log("RÁ LEHET KATTINTANI");
-            this.#lepes++;
-            console.log(this.#lepes);
+            this.#ellenorzes();
         })
-        this.#ellenorzes();
+        
+
+        
     }
 
     #setAllapotLISTA(lampa) {
@@ -41,8 +41,11 @@ class Jatekter {
             aktAllapot = true;
             //console.log(lampa);
             lampa.setAllapot(aktAllapot);
+            this.#db++;
+            console.log("db: " + this.#db);
         } else {
-            aktAllapot = false;
+            aktAllapot = false;            
+            //console.log("db: " + this.#db);
             lampa.setAllapot(aktAllapot);
         }
     }
@@ -50,30 +53,35 @@ class Jatekter {
     #szomszedokKeresese(id) {
     }
 
-    #init(lampa, szamlalo) {
+    #init(lampa) {
         //console.log(this.#allapotLISTA);
-        let aktAllapot;
-
-        //for (let index = 0; index < this.#meret; index++) {
+        let aktAllapot;        
         let esely = Math.floor((Math.random() * 4) + 1);
-        //alapállapot
+        
         if (esely == 1) {
             aktAllapot = true;
             //console.log(lampa);
             lampa.setAllapot(aktAllapot);
+            this.#db++;
+            console.log("69.sor db: " + this.#db);
 
             console.log(aktAllapot);
         } else {
             aktAllapot = false;
             lampa.setAllapot(aktAllapot);
         }
-        //}
+        
     }
 
-    #ellenorzes() {
-        //számláld meg a zöld lámpákat
-        this.#db = 0;
+    #ellenorzes() {     
+        const eredmeny = $(".leLampaSzamlalo");
         
+        this.#allapotLISTA.length;
+
+        console.log(this.#db);
+        eredmeny.empty();   
+        eredmeny.append(`<p>${this.#db}</p>`);
+
     }
 }
 export default Jatekter;
--- JS/Classok/Lampa.js
+++ JS/Classok/Lampa.js
@@ -1,5 +1,5 @@
 class Lampa {
-    #allapot = false;
+    #allapot;
     #id;
     #divELEM;
 
@@ -7,16 +7,14 @@ class Lampa {
         this.#id = id;
         this.#allapot = allapot;
         szuloELEM.append(`<div class="korELEM"></div>`);
-
         this.korELEM = $(".ideJatekter .korELEM:last-child");
-        //console.log(this.korELEM);
 
         this.korELEM.on("click", () => {
             console.log("A kattintást érzékeli")
             this.#kattintasTrigger();
         })
         this.setAllapot(this.#allapot);
-        
+
     }
     setAllapot(aktAllapot) {
         this.#allapot = aktAllapot;
@@ -27,17 +25,12 @@ class Lampa {
         if (this.#allapot == true) {
             this.korELEM.css(`background-color`, `darkgreen`);
             this.#allapot = true;
-            //console.log("igaz ágba bemegy");
 
         } else {
-            //console.log("hamis ágba bemegy");
             this.korELEM.css(`background-color`, `orange`);
             this.allapot = false;
 
         }
-
-        //return this.korELEM.css(`background-color`, szin);
-
     }
 
     getAllapot() {
